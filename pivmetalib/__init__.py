ee0d60499708c0b5b3a1795972ee62c363899dcb
Matthias Probst <matth.probst@gmail.com>
2024-01-25 20:37:09 +0100

    bugfixes

--- pivmetalib/__init__.py
+++ pivmetalib/__init__.py
@@ -1,12 +1,10 @@
 from ._version import __version__
 from .owl import Thing
-from .m4i import ProcessingStep, Variable, Method
 from .utils import get_cache_dir
 
 CONTEXT = "https://raw.githubusercontent.com/matthiasprobst/pivmeta/main/pivmeta_context.jsonld"
 CACHE_DIR = get_cache_dir()
 __all__ = ('__version__',
-           'ProcessingStep',
            'Thing',
            'CONTEXT',
            'CACHE_DIR',
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -8,7 +8,7 @@ import unittest
 from datetime import datetime
 
 import pivmetalib
-from pivmetalib import pivmeta, prov
+from pivmetalib import pivmeta, prov, m4i, owl
 
 __this_dir__ = pathlib.Path(__file__).parent
 CACHE_DIR = pivmetalib.utils.get_cache_dir()
@@ -24,18 +24,18 @@ class TestPIVProcess(unittest.TestCase):
             self.assertIsInstance(p, rdflib.URIRef)
 
     def test_preprocessing_step(self):
-        ps1 = pivmetalib.ProcessingStep(label='p1', start_time=datetime.now())
+        ps1 = m4i.ProcessingStep(name='p1', start_time=datetime.now())
         time.sleep(1)
-        ps2 = pivmetalib.ProcessingStep(label='p2', start_time=datetime.now())
+        ps2 = m4i.ProcessingStep(name='p2', start_time=datetime.now())
 
         ps1.starts_with = ps2
         with self.assertRaises(TypeError):
             ps1.starts_with = 123
         self.assertTrue(ps2.start_time > ps1.start_time)
-        self.assertIsInstance(ps1, pivmetalib.Thing)
-        self.assertIsInstance(ps1, pivmetalib.ProcessingStep)
-        self.assertIsInstance(ps1.starts_with, pivmetalib.Thing)
-        self.assertIsInstance(ps1.starts_with, pivmetalib.ProcessingStep)
+        self.assertIsInstance(ps1, owl.Thing)
+        self.assertIsInstance(ps1, m4i.ProcessingStep)
+        self.assertIsInstance(ps1.starts_with, owl.Thing)
+        self.assertIsInstance(ps1.starts_with, m4i.ProcessingStep)
         self.assertEqual(ps1.starts_with, ps2)
 
         jsonld_string = ps1.dump_jsonld()
@@ -51,9 +51,9 @@ class TestPIVProcess(unittest.TestCase):
         )
         self.assertEqual(mycompany.author.name, 'My GmbH')
         self.assertEqual(mycompany.author.mbox, 'info@mycompany.com')
-        self.assertIsInstance(mycompany, pivmetalib.Thing)
+        self.assertIsInstance(mycompany, owl.Thing)
         self.assertIsInstance(mycompany, pivmeta.PIVSoftware)
-        self.assertIsInstance(mycompany.author, pivmetalib.Thing)
+        self.assertIsInstance(mycompany.author, owl.Thing)
         self.assertIsInstance(mycompany.author, prov.Organization)
 
         mycompany2 = pivmeta.PIVSoftware(
@@ -69,19 +69,19 @@ class TestPIVProcess(unittest.TestCase):
             ],
         )
         self.assertIsInstance(mycompany2.author, list)
-        self.assertIsInstance(mycompany2.author[0], pivmetalib.Thing)
+        self.assertIsInstance(mycompany2.author[0], owl.Thing)
         self.assertIsInstance(mycompany2.author[0], prov.Organization)
         self.assertEqual(mycompany2.author[0].name, 'My GmbH')
         self.assertEqual(mycompany2.author[0].mbox, 'info@mycompany.com')
-        self.assertIsInstance(mycompany2.author[1], pivmetalib.Thing)
+        self.assertIsInstance(mycompany2.author[1], owl.Thing)
         self.assertIsInstance(mycompany2.author[1], prov.Person)
-        self.assertIsInstance(mycompany2, pivmetalib.Thing)
+        self.assertIsInstance(mycompany2, owl.Thing)
         self.assertIsInstance(mycompany2, pivmeta.PIVSoftware)
 
     def test_variable(self):
-        var1 = pivmetalib.Variable(value=4.2)
-        self.assertIsInstance(var1, pivmetalib.Thing)
-        self.assertIsInstance(var1, pivmetalib.Variable)
+        var1 = m4i.Variable(value=4.2)
+        self.assertIsInstance(var1, owl.Thing)
+        self.assertIsInstance(var1, m4i.Variable)
         self.assertEqual(var1.value, 4.2)
 
         jsonld_string = var1.dump_jsonld()
@@ -90,21 +90,21 @@ class TestPIVProcess(unittest.TestCase):
 
     def test_method_no_parameters(self):
         # method without parameters:
-        method1 = pivmetalib.Method(label='method1')
-        self.assertIsInstance(method1, pivmetalib.Thing)
-        self.assertIsInstance(method1, pivmetalib.Method)
-        self.assertEqual(method1.label, 'method1')
+        method1 = m4i.Method(name='method1')
+        self.assertIsInstance(method1, owl.Thing)
+        self.assertIsInstance(method1, m4i.Method)
+        self.assertEqual(method1.name, 'method1')
 
         jsonld_string = method1.dump_jsonld()
         self.check_jsonld_string(jsonld_string)
 
     def test_method_one_parameters(self):
         # method with 1 parameter:
-        var1 = pivmetalib.Variable(value=4.2)
-        method2 = pivmetalib.Method(label='method2', has_parameter=var1)
-        self.assertIsInstance(method2, pivmetalib.Thing)
-        self.assertIsInstance(method2, pivmetalib.Method)
-        self.assertEqual(method2.label, 'method2')
+        var1 = m4i.Variable(value=4.2)
+        method2 = m4i.Method(name='method2', has_parameter=var1)
+        self.assertIsInstance(method2, owl.Thing)
+        self.assertIsInstance(method2, m4i.Method)
+        self.assertEqual(method2.name, 'method2')
         self.assertEqual(method2.has_parameter, var1)
 
         jsonld_string = method2.dump_jsonld()
@@ -113,12 +113,12 @@ class TestPIVProcess(unittest.TestCase):
 
     def test_method_n_parameters(self):
         # method with 2 parameters:
-        var1 = pivmetalib.Variable(value=4.2)
-        var2 = pivmetalib.Variable(value=5.2)
-        method3 = pivmetalib.Method(label='method3', has_parameter=[var1, var2])
-        self.assertIsInstance(method3, pivmetalib.Thing)
-        self.assertIsInstance(method3, pivmetalib.Method)
-        self.assertEqual(method3.label, 'method3')
+        var1 = m4i.Variable(value=4.2)
+        var2 = m4i.Variable(value=5.2)
+        method3 = m4i.Method(name='method3', has_parameter=[var1, var2])
+        self.assertIsInstance(method3, owl.Thing)
+        self.assertIsInstance(method3, m4i.Method)
+        self.assertEqual(method3.name, 'method3')
         self.assertIsInstance(method3.has_parameter, list)
         self.assertEqual(method3.has_parameter, [var1, var2])
 
@@ -135,18 +135,32 @@ class TestPIVProcess(unittest.TestCase):
         sn2 = ssnolib.StandardName(standard_name='y_velocity',
                                    description='y component of velocity',
                                    canonical_units='m s-1')
-        var1 = pivmetalib.Variable(value=4.2, standard_name=sn1)
-        var2 = pivmetalib.Variable(value=5.2, standard_name=sn2)
-        self.assertIsInstance(var1, pivmetalib.Thing)
-        self.assertIsInstance(var1, pivmetalib.Variable)
+        var1 = m4i.Variable(value=4.2, standard_name=sn1)
+        var2 = m4i.Variable(value=5.2, standard_name=sn2)
+        self.assertIsInstance(var1, owl.Thing)
+        self.assertIsInstance(var1, m4i.Variable)
+        self.assertIsInstance(var2, m4i.Variable)
         self.assertEqual(var1.value, 4.2)
-        self.assertEqual(var1.standard_name, sn1)
+        with self.assertRaises(AttributeError):
+            self.assertEqual(var1.standard_name, sn1)
+
+        sn1 = ssnolib.StandardName(standard_name='x_velocity',
+                                   description='x component of velocity',
+                                   canonical_units='m s-1')
+        sn2 = ssnolib.StandardName(standard_name='y_velocity',
+                                   description='y component of velocity',
+                                   canonical_units='m s-1')
+        var1 = pivmeta.Variable(value=4.2, standard_name=sn1)
+        var2 = pivmeta.Variable(value=5.2, standard_name=sn2)
+        self.assertIsInstance(var1, owl.Thing)
+        self.assertIsInstance(var1, pivmeta.Variable)
+        self.assertEqual(var1.value, 4.2)
 
         with self.assertRaises(pydantic.ValidationError):
             var1.standard_name = 123
         var1.standard_name = sn1
 
-        method = pivmetalib.Method(label='method1')
+        method = m4i.Method(name='method1')
         method.has_parameter = [var1, var2]
 
         jsonld_string = method.dump_jsonld()
