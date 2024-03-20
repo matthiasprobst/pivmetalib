1810497e56916694ddb459e15b51d55ac5ee2d1d
Matthias Probst <matth.probst@gmail.com>
2024-03-20 20:51:19 +0100

    make TextVariable available

--- pivmetalib/m4i/__init__.py
+++ pivmetalib/m4i/__init__.py
@@ -1,10 +1,11 @@
 from .method import Method
 from .processingstep import ProcessingStep
 from .tool import Tool
-from .variable import NumericalVariable
+from .variable import NumericalVariable, TextVariable
 
 __all__ = ('ProcessingStep',
            'NumericalVariable',
+           'TextVariable',
            'Method',
            'Tool'
            )
